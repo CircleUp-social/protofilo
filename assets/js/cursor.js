2eaf8a07db8468bfa1d01a3a66d90d97ec791d98
CircleUp-social <kaifsoftwarengineer@gmail.com>
2024-12-16 18:40:07 +0600

    Update cursor.js

--- assets/js/cursor.js
+++ assets/js/cursor.js
@@ -1,19 +1,6 @@
 // Folder path and cursor files
 const cursorImages = [
-    './assets/cursor/cur1.png',
-    './assets/cursor/cur2.png',
-    './assets/cursor/cur3.png',
-    './assets/cursor/cur4.png',
-    './assets/cursor/cur5.png',
-    './assets/cursor/cur6.png',
-    './assets/cursor/cur7.png',
-    './assets/cursor/cur8.png',
-    './assets/cursor/cur9.png',
-    './assets/cursor/cur10.png',
-    './assets/cursor/cur11.png',
-    './assets/cursor/cur12.png',
-    './assets/cursor/cur13.png',
-    './assets/cursor/cur14.png'
+    './assets/cursor/cur2.png'
 ];
 
 // Function to change cursor
@@ -29,5 +16,5 @@ function changeCursor() {
 // Set the initial cursor immediately on page load
 changeCursor();
 
-// Change cursor every 10 seconds (10000 milliseconds)
+// Change cursor every 10 seconds (10000000 milliseconds)
 setInterval(changeCursor, 10000);
